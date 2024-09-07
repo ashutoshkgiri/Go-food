dedb02a840656b001cf64a3fe51ddf4f8c643590
ashutoshkgiri <143159922+ashutoshkgiri@users.noreply.github.com>
2024-09-07 22:25:02 +0530

    ~

---
+++ GoFood-main/backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://localhost:27017/fsproject';
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("foodData");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+        }
+    })
+};
